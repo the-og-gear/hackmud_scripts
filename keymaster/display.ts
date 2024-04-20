5dcb133550c1548bd0b4882696982fc7307783b2
Gear <gear@gear.is>
2024-04-20 10:54:47 -0400

    Anonymous data collection implementation

--- keymaster/display.ts
+++ keymaster/display.ts
@@ -21,6 +21,19 @@ function create_table(index: number, rarity: number, name: string, k3y: string) 
     return index.toString().padStart(3, "0").padStart(5, " ") + " | `" + rarity_colors[rarity] + name + "_" + k3y + "`";
 }
 
+function update_db(k: Upgrade[]): void {
+    const current_db = $db.f({SID:"keymaster_k3ys"}).first();
+    const db_k3ys = JSON.parse(current_db.k3ys.toString());
+    for (const k3y of k) {
+        if (!db_k3ys[k3y.k3y as string])
+            db_k3ys[k3y.k3y as string] = [];
+        if (db_k3ys[k3y.k3y as string].includes(k3y.rarity))
+            continue;
+        db_k3ys[k3y.k3y as string].push(k3y.rarity);
+    }
+    $db.u1({SID:"keymaster_k3ys"}, {$set:{k3ys:JSON.stringify(db_k3ys,undefined,0)}});
+}
+
 export default(c: Context, a: {u: Scriptor}) => {
     if (c.calling_script || (c as ScriptorContext).is_scriptor)
         return "`DYou cannot call this from another script!`";
@@ -32,13 +45,16 @@ export default(c: Context, a: {u: Scriptor}) => {
     if (!upgrades)
         return "`DCommand failed.`";
     let k3ys = [];
+    let real_k3ys = [];
     for (const u of upgrades as Upgrade[]) {
         if (u.name.includes("k3y")) {
             k3ys.push(create_table(u.i, u.rarity, u.name, u.k3y as string));
+            real_k3ys.push(u);
         }
     }
     if (!(k3ys.length > 0))
         return "`DUser has no k3ys! :(`";
+    update_db(real_k3ys);
     k3ys = sort_k3ys(k3ys);
     k3ys.unshift("Index | k3y", "------+--------------");
     return k3ys;
